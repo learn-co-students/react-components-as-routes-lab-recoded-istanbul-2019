cadd563bb5fb4fc5fae157236ee845e3e304e9e5
Mohammed <github email address>
2020-03-06 20:35:46 +0300

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,14 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+        <NavBar/>
+        <Route path="/" component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+
+      </div>
     </Router>
   );
 };
