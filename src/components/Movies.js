457b6d77bba0fb85bd5c0a4f1036421adc8e77f0
Recep Öztürk <turkishteacher23@gmail.com>
2020-03-08 18:16:05 +0000

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,8 +5,16 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
-    </div>
-  );
+        <h1>Movies Page</h1>
+        {movies.map(movie => (
+          <div>
+          <h2>{movie.title}</h2>
+          <p>{movie.time}</p>
+          <ul>{movie.genres.map(genre => <li>{genre}</li>)}</ul>
+          </div>
+        ))}
+      </div>
+  )
 };
 
 export default Movies;
--- src/containers/App.js
+++ src/containers/App.js
@@ -14,6 +14,13 @@ const App = (props) => {
   return (
     <Router>
       {/*{code here}*/}
+      <div>
+      <NavBar />
+      <Route path="/" component={Home} />
+      <Route path="/movies" component={Movies} />
+      <Route path="/directors" component={Directors} />
+      <Route path="/actors" component={Actors} />
+      </div>
     </Router>
   );
 };
