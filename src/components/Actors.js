abf63065b0f7b82e66c668fd219ce15b428fc0e4
rashaabdulrazzak <rasha.abdulrazzak@gmail.com>
2020-03-14 08:29:27 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,11 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+        {/*{code here}*/}
+        <h1>Actors Page</h1>     
+        {actors.map(actor=>{ return <div key={Math.random()}> <h3>{actor.name} </h3><ul>
+          {actor.movies.map(gen => {return <li  key={Math.random()}>{gen}</li>})}
+          </ul></div>})}      
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,10 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1>Movies Page</h1>     
+        {movies.map(movie=>{ return <div key={Math.random()}> <h3>{movie.title} </h3> <p> {movie.time}</p><ul>
+          {movie.genres.map(gen => {return <li  key={Math.random()}>{gen}</li>})}
+          </ul></div>})}      
     </div>
   );
 };
