387db892a1d8999227b75f7e06220298b5b66593
Halit Batur <halitfuatbatur@gmail.com>
2020-06-01 17:44:22 +0300

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { movies } from '../data';
+import { render } from 'enzyme';
 
 const Movies = () => {
+
+  const renderd = movies.map((movie, index) => {
+    return <div key={index}>
+      <h3>{movie.title}</h3>
+      <p>{movie.time}</p>
+      {movie.genres.map((genre, index) => <ul key={index}>{genre}</ul>)}
+      </div>
+  })
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+        {renderd}
     </div>
   );
 };
