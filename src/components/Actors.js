fee191b524ba263b94a025d04c6a2d66babb1811
tugba <ttugbasen@gmail.com>
2020-06-14 21:37:23 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,18 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const renderActors = () => {
+    return actors.map((actor, i) => {
+    return <div key={i}>
+      <h3>{actor.name}</h3>
+      {actor.movies.map((movie, i) => <ul key={i}>{movie}</ul>)}
+      </div>
+  })
+  };
   return (
     <div>
-      {/*{code here}*/}
+        <h1>Actors Page</h1>
+        {renderActors()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { movies } from '../data';
 
-const Movies = () => {
+const Movies = (props) => {
+
+  const renderMovies = () => {
+    return movies.map((movie, i) => {
+    return <div key={i}>
+      <h3>{movie.title}</h3>
+      <h3>{movie.time}</h3>
+      {movie.genres.map((genre, i) => <ul key={i}>{genre}</ul>)}
+      </div>
+  })
+  };
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
