34070c45867f2b62b1ac041762c4f25a217b6def
bahaadabbagh <bahaa.dabbagh@gmail.com>
2020-06-24 13:23:41 +0300

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  let actorsInfo =()=>{
+    let oneDirector = directors.map(function(info, index){
+      return  <div key={index}>
+      <h3>Name: {info.name}</h3>
+      <p>Movies:</p>
+      <ul>
+          {info.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+              ))}
+              </ul>
+          </div>
+    })
+    return oneDirector
+  }
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {actorsInfo()}
     </div>
   );
 }
