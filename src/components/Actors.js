16c92f2e7759bad2decdb6f34d1578b2e0fd5809
refica <refia.candan@gmail.com>
2020-06-27 15:41:00 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const renderActors = actors.map((actor, index) => {
+    return (
+      <div key={index}>
+        <p className="actor">{actor.name}</p>
+        {actor.movies.map((movie, i) => {
+            return (
+              <ul>
+                <li key={i}>{movie}</li>
+              </ul>
+            )
+          })}
+      </div>
+    )
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { movies } from '../data';
+import { render } from 'enzyme';
 
 const Movies = () => {
+  
+  const renderMovies = movies.map((movie, index) => {
+      return (
+        <div key={index}>
+          <h3>{movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <p>Genres: {movie.genres.map((genre, i) => {
+            return (
+              <ul>
+                <li key={i}>{genre}</li>
+              </ul>
+            )
+          })}
+          </p>
+        </div>
+      )
+    })
+  
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies}
     </div>
   );
 };
